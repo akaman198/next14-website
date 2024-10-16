d200daf4a2ba44e5dec7177d6e6b92050b5889f5
Mwanambuyu Akamandisa <amwaka98@gmail.com>
2024-10-16 17:32:09 +0200

    add links array

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import Link from "next/link";
 
 const Navbar = () => {
-  return <div>navbar</div>;
+  return (
+    <div>
+      <div>logo</div>
+      <div>
+        <Link href="/">Homepage</Link>
+        <Link href="/about">About</Link>
+        <Link href="/contact">Contact</Link>
+        <Link href="/blog">Blog</Link>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
