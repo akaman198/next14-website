b6eabacf99db08fbf0d9e397a27b24c2593370e4
Mwanambuyu Akamandisa <amwaka98@gmail.com>
2024-10-18 12:14:28 +0200

    add footer info

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import styles from "./footer.module.css";
 
 const Footer = () => {
-  return <div>footer</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.logo}>akamanDev</div>
+      <div className={styles.text}>
+        copyright @2024 akaman ~ all rights reservered
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
