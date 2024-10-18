ff4f35e6d433e3c8824f1c090d5fffac840685d0
Mwanambuyu Akamandisa <amwaka98@gmail.com>
2024-10-18 17:34:42 +0200

    add single post layout and basic responsiveness to all pages

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,7 +1,39 @@
 import React from "react";
+import styles from "./singlePost.module.css";
+import Image from "next/image";
 
 const SinglePostPage = () => {
-  return <div>single blog page</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image src="/about.png" alt="" fill className={styles.img} />
+      </div>
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>Title</h1>
+        <div className={styles.detail}>
+          <Image
+            src="/about.png"
+            alt=""
+            width={50}
+            height={50}
+            className={styles.avater}
+          />
+          <div className={styles.detailText}>
+            <span className={styles.detailTitle}>Author</span>
+            <span className={styles.detailValue}>Terry Cruse</span>
+          </div>
+          <div className={styles.detailText}>
+            <span className={styles.detailTitle}>Published</span>
+            <span className={styles.detailValue}>06.10.23</span>
+          </div>
+        </div>
+        <div className={styles.content}>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi at
+          necessitatibus non ducimus libero animi.
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SinglePostPage;
--- src/components/postCard/postCard.jsx
+++ src/components/postCard/postCard.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Image from "next/image";
 import styles from "./postCard.module.css";
 import Link from "next/link";
