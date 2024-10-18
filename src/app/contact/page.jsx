c449710f639e19dc9a4e25ea06de64cd609887a0
Mwanambuyu Akamandisa <amwaka98@gmail.com>
2024-10-18 13:26:11 +0200

    add contact page info

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,7 +1,35 @@
 import React from "react";
+import styles from "./contact.module.css";
+import Image from "next/image";
 
 const contact = () => {
-  return <div>contact page</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image
+          src="/contact.png"
+          alt="contact image"
+          fill
+          className={styles.contactImg}
+        />
+      </div>
+      <div className={styles.formContainer}>
+        <form action="" className={styles.form}>
+          <input type="text" placeholder="Name and Surname" />
+          <input type="email" placeholder="Email Address" />
+          <input type="number" placeholder="Phone number (optional)" />
+          <textarea
+            name=""
+            cols={30}
+            rows={5}
+            id=""
+            placeholder="Message"
+          ></textarea>
+          <button className={styles.button}>Send</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default contact;
